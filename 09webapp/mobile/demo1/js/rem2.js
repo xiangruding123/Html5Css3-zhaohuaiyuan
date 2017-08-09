eb6d0509681fda2b4ad00d952cb038ab9cd6d2a9
xiangruding <18753377090@163.com>
2017-08-09 13:21:10 +0800

    09webapp&10login

---
+++ 09webapp/mobile/demo1/js/rem2.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2016/9/28 0028.
+ */
+(function(win) {
+    var doc = win.document;
+    var docEl = doc.documentElement;
+    var tid;
+
+    function refreshRem() {
+        var width = docEl.getBoundingClientRect().width;
+        if (width > 540) { // 最大宽度
+            width = 540;
+        }
+        var rem = width / 10; // 将屏幕宽度分成10份， 1份为1rem
+        docEl.style.fontSize = rem + 'px';
+    }
+
+    win.addEventListener('resize', function() {
+        clearTimeout(tid);
+        tid = setTimeout(refreshRem, 300);
+    }, false);
+    win.addEventListener('pageshow', function(e) {
+        if (e.persisted) {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 300);
+        }
+    }, false);
+
+    refreshRem();
+
+})(window);
